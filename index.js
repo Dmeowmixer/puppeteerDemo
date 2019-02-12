6c6f8d227b4ac098aa98d82b5c447df22ebbe67a
Dmeowmixer <ransonikuo@gmail.com>
2019-02-12 11:14:23 -1000

    refactors to utilize puppeteer, targeting reddit

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
-const rp = require('request-promise');
+const puppeteer = require('puppeteer');
+const $ = require('cheerio');
 const url = 'https://www.reddit.com';
 
-rp(url)
-  .then(function(){
-    console.log(html);
+puppeteer
+  .launch()
+  .then(function(browser){
+    return browser.newPage()
+    .then(function(page){
+      return page.goto(url).then(function(){
+        return page.content
+      })
+    })
+    .then(function(html){
+      
+      console.log(html);
+    })
+    .catch(function(err){
+      // Err handler
+    })
   })
-  .catch(function(err){
-
-  });
+// Ends Puppeteer block
