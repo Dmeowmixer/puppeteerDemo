c1846eae4621b592a7e67ba81d306ce0510c414a
Dmeowmixer <ransonikuo@gmail.com>
2019-02-12 11:20:32 -1000

    utilizing async await

--- index.js
+++ index.js
@@ -2,21 +2,10 @@ const puppeteer = require('puppeteer');
 const $ = require('cheerio');
 const url = 'https://www.reddit.com';
 
-puppeteer
-  .launch()
-  .then(function(browser){
-    return browser.newPage()
-    .then(function(page){
-      return page.goto(url).then(function(){
-        return page.content
-      })
-    })
-    .then(function(html){
-      
-      console.log(html);
-    })
-    .catch(function(err){
-      // Err handler
-    })
-  })
-// Ends Puppeteer block
+(async() => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto(url);
+  await page.screenshot({path: 'snap.png'});
+  await browser.close();
+})
